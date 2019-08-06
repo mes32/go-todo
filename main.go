18c9db21c3237e6f8a96c14ac778e95d0d025b47
mes32 <stockman.mike@gmail.com>
2019-08-06 17:51:42 -0500

    Refactor main.go

--- main.go
+++ main.go
@@ -9,19 +9,19 @@ import (
 )
 
 func main() {
-	// http.HandleFunc("/", rootRouter)
+	const BUILD_DIR = "./client/build/"
+
+	clientRouter := http.FileServer(http.Dir(BUILD_DIR))
+	http.Handle("/", clientRouter)
 	http.HandleFunc("/api/task/", routers.TaskRouter)
 	http.HandleFunc("/api/task-group/", routers.GroupRouter)
-
-	buildHandler := http.FileServer(http.Dir("./client/build/"))
-	http.Handle("/", buildHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
-	println("Starting server on port: " + port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	log.Println("Starting server on port: " + port)
+	if err := http.ListenAndServe(":" + port, nil); err != nil {
 		log.Panic(err)
 	}
 }
