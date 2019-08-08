753e0c6ce81cd1c2e1b7f994c27b867b306dba42
mes32 <stockman.mike@gmail.com>
2019-08-08 14:02:37 -0500

    Allow user to mark tasks complete by clicking anywhere in row

--- client/src/components/TodoListTask.js
+++ client/src/components/TodoListTask.js
@@ -6,12 +6,12 @@ import CheckBox from './CheckBox';
 import Task from '../classes/Task';
 
 function TodoListTask({ task, color, completeTask }) {
-    const onChange = () => {
+    const handleChange = () => {
         completeTask(task.id, !task.complete);
     }
     return (
-        <StyledDiv complete={task.complete}>
-            <CheckBox checked={task.complete} onChange={onChange} color={color} /> <span className="description">{task.description}</span>
+        <StyledDiv complete={task.complete} onClick={handleChange}>
+            <CheckBox checked={task.complete} onChange={handleChange} color={color} /> <span className="description">{task.description}</span>
         </StyledDiv>
     )
 }
@@ -24,6 +24,7 @@ TodoListTask.propTypes = {
 
 const StyledDiv = styled.div`
     display: block;
+    cursor: pointer;
     margin: 0 0 1.2rem 4px;
     font-size: 1.2rem;
     color: ${props => props.complete ? 'gray' : 'black'};
