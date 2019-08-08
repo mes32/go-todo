f1df0d57190ab821c24493f4ac1d45eab99113b5
mes32 <stockman.mike@gmail.com>
2019-08-07 21:53:47 -0500

    Refactor the group handler

--- server/routers/group.go
+++ server/routers/group.go
@@ -15,36 +15,48 @@ type TaskGroupRequest = types.TaskGroupRequest
 func GroupRouter(writer http.ResponseWriter, request *http.Request) {
 	switch request.Method {
 	case http.MethodPost:
-		bytes, err := ioutil.ReadAll(request.Body)
-		defer request.Body.Close()
-		if err != nil {
-			log.Println(err)
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		var taskGroup TaskGroupRequest
-		err = json.Unmarshal(bytes, &taskGroup)
-		if err != nil {
-			log.Println(err)
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		err = database.CreateGroup(taskGroup.Name)
-		if err != nil {
-			log.Println(err)
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		writer.WriteHeader(http.StatusCreated)
-		writer.Write([]byte("201 - Created"))
+		createGroup(writer, request)
 	case http.MethodDelete:
-		writer.WriteHeader(http.StatusOK)
-		writer.Write([]byte("200 - OK"))
+		deleteGroup(writer)
 	default:
-		writer.WriteHeader(http.StatusInternalServerError)
-		writer.Write([]byte("500 - Internal Server Error"))
+		defaultError(writer)
 	}
+}
+
+func createGroup(writer http.ResponseWriter, request *http.Request) {
+	bytes, err := ioutil.ReadAll(request.Body)
+	defer request.Body.Close()
+	if err != nil {
+		log.Println(err)
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var taskGroup TaskGroupRequest
+	err = json.Unmarshal(bytes, &taskGroup)
+	if err != nil {
+		log.Println(err)
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = database.CreateGroup(taskGroup.Name)
+	if err != nil {
+		log.Println(err)
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	writer.WriteHeader(http.StatusCreated)
+	writer.Write([]byte("201 - Created"))
+}
+
+func deleteGroup(writer http.ResponseWriter) {
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("200 - OK"))
+}
+
+func defaultError(writer http.ResponseWriter) {
+	writer.WriteHeader(http.StatusInternalServerError)
+	writer.Write([]byte("500 - Internal Server Error"))
 }
